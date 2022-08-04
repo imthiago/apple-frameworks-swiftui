fba9aeaa4ed87498039f6cd354d61fdedc03e30e
Thiago Oliveira <thiago@outlook.com.br>
2022-08-03 23:42:01 -0300

    Moving grid view to a new file

--- Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
@@ -18,6 +18,9 @@ struct FrameworkGridView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(MockData.frameworks) {
                         FrameworkTitleView(framework: $0)
+                            .onTapGesture {
+                                print("")
+                            }
                     }
                 }
             }
@@ -30,23 +33,5 @@ struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
             .preferredColorScheme(.dark)
-    }
-}
-
-struct FrameworkTitleView: View {
-    let framework: Framework
-
-    var body: some View {
-        VStack {
-            Image(framework.imageName)
-                .resizable()
-                .frame(width: 90, height: 90)
-            Text(framework.name)
-                .font(.title2)
-                .fontWeight(.semibold)
-                .scaledToFit()
-                .minimumScaleFactor(0.6)
-        }
-        .padding()
     }
 }
