8b332ee7574018d7add16b3988e31495c1083e3a
Thiago Oliveira <thiago@outlook.com.br>
2022-08-04 00:12:45 -0300

    UIKit integration

--- Apple-Frameworks/Apple-Frameworks/FrameworkDetailView.swift
+++ Apple-Frameworks/Apple-Frameworks/FrameworkDetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct FrameworkDetailView: View {
     var framework: Framework
     @Binding var isShowingDetailsView: Bool
+    @State private var isShowingSafariView = false
 
     var body: some View {
         VStack {
@@ -38,10 +39,13 @@ struct FrameworkDetailView: View {
             Spacer()
 
             Button {
-
+                isShowingSafariView = true
             } label: {
                 AFButton(title: "Learn More")
             }
+        }
+        .fullScreenCover(isPresented: $isShowingSafariView) {
+            SafariView(url: URL(string: framework.urlString)!)
         }
     }
 }
